a2e321e041ced8716b7463af11a3807efc7ed13d
a-yerzhyk <andriiyerzh@gmail.com>
2023-10-25 23:30:33 +0300

    fix: export Battle class

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './classes/Game.js';
 export * from './classes/EventsManager.js';
+export * from './classes/Battle.js';
 export * from './types/types.js';
 export * from './config/config.js';
